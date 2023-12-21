b35f2fbcbd5bf91b943ccbb1041c9ee5edf7c9b8
Levente Berky <levente.berky@gmail.com>
2023-12-21 20:52:42 +0100

    Add which key default config to nvim

---
+++ nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end
+  -- TODO: we can define here our own keybinding helper
+}
