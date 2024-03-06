66681d27cf3c30666c7259cf6a31351389e137fe
Levente Berky <levente.berky@gmail.com>
2024-03-06 10:45:54 +0100

    add lsp shortcuts to nvim

--- dotfiles/home/nvim/lsp.nix
+++ dotfiles/home/nvim/lsp.nix
@@ -10,6 +10,70 @@
         mode = [ "n" ];
         options.desc = "Format file";
       }
+      {
+        action = "vim.diagnostic.open_float";
+        key = "<leader>e";
+        lua = true;
+        mode = [ "n" ];
+        options.desc = "Open floating errors";
+      }
+      {
+        action = "vim.diagnostic.goto_prev";
+        key = "<leader>k";
+        lua = true;
+        mode = [ "n" ];
+        options.desc = "Go to prev error";
+      }
+      {
+        action = "vim.diagnostic.goto_next";
+        key = "<leader>j";
+        lua = true;
+        mode = [ "n" ];
+        options.desc = "Go to next error";
+      }
+      {
+        action = "vim.lsp.buf.hover";
+        key = "K";
+        lua = true;
+        mode = [ "n" ];
+        options.desc = "Hover";
+      }
+      {
+        action = "vim.lsp.buf.declaration";
+        key = "gD";
+        lua = true;
+        mode = [ "n" ];
+        options.desc = "Go to declaration";
+      }
+      {
+        action = "vim.lsp.buf.definition";
+        key = "gd";
+        lua = true;
+        mode = [ "n" ];
+        options.desc = "Go to definition";
+      }
+      {
+        action = "vim.lsp.buf.references";
+        key = "gr";
+        lua = true;
+        mode = [ "n" ];
+        options.desc = "Search for references";
+      }
+      {
+        action = "vim.lsp.buf.rename";
+        key = "<leader>rn";
+        lua = true;
+        mode = [ "n" ];
+        options.desc = "Rename variable in buffer";
+      }
+      {
+        action = "vim.lsp.buf.code_action";
+        key = "<leader>ca";
+        lua = true;
+        mode = [ "n" "v" ];
+        options.desc = "Code action";
+      }
+ 
     ];
     plugins = { 
       lsp = {
