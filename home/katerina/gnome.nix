b567dd3a21744444ed20ad34c6d9117cbe9d0cc9
Levente Berky <levente.berky@gmail.com>
2024-09-03 14:34:05 +0200

    Fix profile photo and extensions for katerina

--- home/katerina/gnome.nix
+++ home/katerina/gnome.nix
@@ -3,7 +3,6 @@
 {
   home.packages = [
     pkgs.gnomeExtensions.user-themes
-    pkgs.gnomeExtensions.paperwm
     pkgs.gnomeExtensions.switcher
   ];
 
@@ -23,7 +22,7 @@
     };
   };
 
-  home.file.".face".source = ../../wallpapers/sebastian-portrait.png;
+  home.file.".face".source = ../../wallpapers/emily-portrait.png;
 
   dconf.settings = {
     "org/gnome/desktop/interface" = {
@@ -58,21 +57,6 @@
         "user-theme@gnome-shell-extensions.gcampax.github.com" 
         "switcher@landau.fi"
       ];
-    };
-
-    "org/gnome/shell/extensions/paperwm" = {
-      default-focus-mode = 0;
-      disable-scratch-in-overview = true;
-      disable-topbar-styling = false;
-      gesture-horizontal-fingers = 4;
-      only-scratch-in-overview = false;
-      open-window-position = 0;
-      overview-ensure-viewport-animation = 1;
-      restore-attach-modal-dialogs = "false";
-      restore-edge-tiling = "false";
-      show-focus-mode-icon = true;
-      show-window-position-bar = false;
-      use-default-background = true;
     };
    
     "org/gnome/shell/extensions/switcher" = {
