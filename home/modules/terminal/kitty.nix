4d227bd43cc722ec74ea733075d547f0b696ee23
Levente Berky <levente.berky@gmail.com>
2024-09-04 10:43:44 +0200

    Fix terminals again

--- home/modules/terminal/kitty.nix
+++ home/modules/terminal/kitty.nix
@@ -3,7 +3,6 @@
     enable = true;
     package = pkgs.unstable.kitty;
     shellIntegration.enableZshIntegration = true;
-    theme = "Catppuccin-Mocha";
     settings = {
       hide_window_decorations = true;
     };
