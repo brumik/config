a3f1c382f8684c35f72686b5f25915e9408ec619
Levente Berky <levente.berky@gmail.com>
2024-10-28 08:22:42 +0100

    WIP

--- system/n100-config.nix
+++ system/n100-config.nix
@@ -6,5 +6,8 @@
     ./modules/stylix-everforest.nix
   ];
 
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 0;
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
   networking.hostName = "nixos-n100"; # Define your hostname.
 }
