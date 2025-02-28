b6e8e9234c7d4764048c62c811b17e1b41c84eb4
Levente Berky <levente.berky@gmail.com>
2025-02-28 10:02:15 +0100

    Fix the anteater config (and deploy)

--- hosts/anteater/default.nix
+++ hosts/anteater/default.nix
@@ -29,9 +29,4 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "anteater";
-
-  # Styling
-  stylix = {
-    image = lib.mkForce ../assets/wallpapers/anteater-3360x2240.jpg;
-  };
 }
