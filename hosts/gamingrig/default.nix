9efd07084b2ea5dbf899569d4088fec0dda9483e
Levente Berky <levente.berky@gmail.com>
2025-04-08 20:52:54 +0200

    Fix gaming rig

--- hosts/gamingrig/default.nix
+++ hosts/gamingrig/default.nix
@@ -1,10 +1,34 @@
-{ ... }: {
+{ config, ... }:
+let uname = "gamer";
+in {
   imports = [
     ./hardware-configuration.nix
 
     ../common/core
-    ../common/users/gamer.nix
   ];
+
+  networking.hostName = "gamingrig";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  hardware.xone.enable = true;
+  #TEMP
+  # # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  # services.xserver.videoDrivers = [ "amdgpu" ];
+  #
+  # # Enable the GNOME Desktop Environment.
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+  # #TEMP
+  #
+  # programs.steam = {
+  #   enable = true;
+  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  #   localNetworkGameTransfers.openFirewall = true; 
+  # };
 
   services.desktopManager.plasma6.enable = true;
 
@@ -14,16 +38,25 @@
       updater.splash = "vendor";
       enable = true;
       autoStart = true;
-      user = "gamer";
+      user = "${uname}";
       desktopSession = "plasma";
     };
-    steamos = {
-      useSteamOSConfig = true;
-    };
+    steamos = { useSteamOSConfig = false; };
   };
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  sops.secrets."brum/hashed-password".neededForUsers = true;
+  # It's important to include users.mutableUsers = false to ensure the user can't modify
+  # their password or groups. Furthermore, if the user had already been created prior to
+  # setting their password this way, their existing password will not be overwritten
+  # unless this option is false.
+  users.mutableUsers = false;
 
-  networking.hostName = "gamingrig";
+  users.users."${uname}" = {
+    uid = 1000;
+    isNormalUser = true;
+    description = "Gamer";
+    extraGroups = [ "networkmanager" "wheel" ];
+    hashedPasswordFile = config.sops.secrets."brum/hashed-password".path;
+  };
+
 }
