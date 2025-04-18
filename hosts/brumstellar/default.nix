7cc37b80532d4b44993c4e6aaea5da82520231e4
Levente Berky <levente.berky@gmail.com>
2025-04-18 19:12:54 +0200

    Add experimental search ui for ollama

--- hosts/brumstellar/default.nix
+++ hosts/brumstellar/default.nix
@@ -10,7 +10,7 @@
     ../common/optional/docker.nix
     ../common/optional/smb.nix
     ../common/optional/nvidia.nix
-    # ../common/optional/ollama.nix
+    ../common/optional/ollama.nix
     ../common/optional/scanner.nix
     ../common/optional/sound.nix
     ../common/optional/gaming.nix
@@ -39,4 +39,16 @@
 
   # Needed for the yubike UI
   services.pcscd = { enable = true; };
+
+  # AI Web UI testing
+  services.open-webui = {
+    enable = true;
+    package = pkgs.unstable.open-webui;
+    port = 11111;
+    environment = {
+      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+      # Disable authentication
+      WEBUI_AUTH = "False";
+    };
+  };
 }
--- hosts/common/optional/ollama.nix
+++ hosts/common/optional/ollama.nix
@@ -3,6 +3,7 @@
   services.ollama = {
     enable = true;
     acceleration = "cuda";
+    port = 11434;
     # included only 25.05 release
     # loadModels = [ "codellama:7b" ];
   };
