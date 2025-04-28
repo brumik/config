21298a7c841cb0e4072106c07e347781cdd93fce
Levente Berky <levente.berky@gmail.com>
2025-04-28 09:33:51 +0200

    Set up properly immich

--- hosts/common/optional/homelab/immich.nix
+++ hosts/common/optional/homelab/immich.nix
@@ -1,14 +1,16 @@
 { config, lib, ... }:
 let cfg = config.homelab.immich;
+  dname = "photos.${config.homelab.domain}";
 in {
   options.homelab.immich = { enable = lib.mkEnableOption "Immich"; };
 
   config = lib.mkIf cfg.enable {
     services.immich = {
       enable = true;
-      # mediaLocation = "/var/lib/immich";
+      mediaLocation = "/mnt/share/immich";
       host = "127.0.0.1";
       port = 2283;
+      group = config.homelab.group;
       environment = {
         IMMICH_TRUSTED_PROXIES = "127.0.0.1";
       };
@@ -24,10 +26,28 @@ in {
     homelab.homepage.app = [{
       Immich = {
         icon = "immich.png";
-        href = "https://photos.${config.homelab.domain}";
-        siteMonitor = "https://photos.${config.homelab.domain}";
+        href = "https://${dname}";
+        siteMonitor = "https://${dname}";
         description = "Google photos at home";
       };
+    }];
+
+    homelab.authelia.oidc.clients = [{
+      client_id = "immich";
+      client_name = "Immich";
+      client_secret =
+        "$pbkdf2-sha512$310000$zdze0iljXy76xeHihU7lbg$FDCjNnLuQ7qpDGzX03zFPuFUyGdiHE3OGEZvbD8/rXUp79HCFnGd1KflgUqWUXtthTRDCBch3IusTMAJzBkqRQ";
+      public = false;
+      consent_mode = "implicit";
+      authorization_policy = "one_factor";
+      require_pkce = false;
+      redirect_uris = [
+        "https://${dname}/auth/login"
+        "https://${dname}/user-settings"
+        "app.immich:///oauth-callback"
+      ];
+      scopes = [ "openid" "email" "profile" ];
+      userinfo_signed_response_alg = "none";
     }];
   };
 }
