a34ec66afa002f5b66ece5bccbec465d81a622f3
Levente Berky <levente.berky@gmail.com>
2025-05-08 14:21:30 +0200

    Restore hardware config for brumstellar

--- hosts/brumstellar/hardware-configuration.nix
+++ hosts/brumstellar/hardware-configuration.nix
@@ -8,9 +8,9 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "ahci" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
