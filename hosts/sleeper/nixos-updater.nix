66300b439bbdaf99834e01bf92d8e89fd19fd2d5
Levente Berky <levente.berky@gmail.com>
2025-06-30 09:14:48 +0200

    Update the update script

--- hosts/sleeper/nixos-updater.nix
+++ hosts/sleeper/nixos-updater.nix
@@ -6,7 +6,6 @@ let
     runtimeInputs = [ pkgs.nix pkgs.git ];
     text = ''
       #!/bin/bash
-
       set -e
 
       # Local directory for the repository
@@ -15,18 +14,22 @@ let
       # Create the directory if it doesn't exist
       mkdir -p "$REPO_DIR"
 
+      echo "Entering the $REPO_DIR"
       cd $REPO_DIR
-
-      git config user.email "sleeper@berky.com"
-      git config user.name "Sleeper Server"
 
       # Clone or pull the repository
       if [ ! -d ".git" ]; then
-        git clone "git@github.com/brumik/config.git" "$REPO_DIR"
+        echo "Cloning the repo"
+        git clone "https://$${GITHUB_TOKEN}@github.com/brumik/config.git" ./
       else
+        echo "Updating the repo"
         git fetch origin
         git reset --hard origin/main
       fi
+
+      echo "Setting up the username and password"
+      git config user.email "sleeper@berky.com"
+      git config user.name "Sleeper Server"
 
       # Update flakes
       nix flake update --accept-flake-config
@@ -35,36 +38,37 @@ let
       nix flake check --accept-flake-config
 
       # Build
-      ${builtins.concatStringsSep "\n\n" (map (host:
-        ''
+      ${builtins.concatStringsSep "\n\n" (map (host: ''
         echo "Building NixOS configuration for: ${host}"
         nix build ".#nixosConfigurations.${host}.config.system.build.toplevel" --accept-flake-config || {
           echo "Failed to build ${host}"
           exit 1
         }
-        ''
-      ) hosts)}
+      '') hosts)}
 
       # Commit and push
+      echo "Build success, pushing the updates to git"
       git add .
       git commit -m "Automated update and build"
       git push origin main
     '';
   };
 in {
-  sops.secrets = { "private-keys/id-n100-github" = { }; };
-
-  programs.ssh.extraConfig = ''
-    Host github.com
-        IdentityFile ${config.sops.secrets."private-keys/id-n100-github".path}
-        IdentitiesOnly yes
-  '';
+  sops.secrets = { "n100/update-flake/github-token" = { }; };
+  sops.templates."n100/update-flake/.env" = {
+    content = ''
+      GITHUB_TOKEN=${config.sops.placeholder."n100/update-flake/github-token"}
+    '';
+  };
 
   systemd.services.update-flake = {
     description = "Run update every Sunday at 8 AM";
+
+
     serviceConfig = {
       Type = "oneshot";
       ExecStart = "${update-flake}/bin/update-flake.sh";
+      EnvironmentFile = config.sops.templates."n100/update-flake/.env".path;
     };
   };
 
