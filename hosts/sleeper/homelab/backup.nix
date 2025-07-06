de7c74d7d9cccaaac2d90ba5f91fa8cdda94d216
Levente Berky <levente.berky@gmail.com>
2025-07-06 18:28:16 +0200

    Fir merge instead of override

--- hosts/sleeper/homelab/backup.nix
+++ hosts/sleeper/homelab/backup.nix
@@ -52,55 +52,58 @@ in {
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    users.users.restic = { isNormalUser = true; };
-    sops.secrets."n100/restic/password" = { owner = "restic"; };
-    sops.secrets."n100/restic/id_hetzner_pub" = { owner = "restic"; };
-    sops.secrets."n100/restic/id_hetzner" = { owner = "restic"; };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      users.users.restic = { isNormalUser = true; };
+      sops.secrets."n100/restic/password" = { owner = "restic"; };
+      sops.secrets."n100/restic/id_hetzner_pub" = { owner = "restic"; };
+      sops.secrets."n100/restic/id_hetzner" = { owner = "restic"; };
 
-    security.wrappers.restic = {
-      source = "${pkgs.restic.out}/bin/restic";
-      owner = "restic";
-      group = "share";
-      permissions = "u=rwx,g=,o=";
-      capabilities = "cap_dac_read_search=+ep";
-    };
+      security.wrappers.restic = {
+        source = "${pkgs.restic.out}/bin/restic";
+        owner = "restic";
+        group = "share";
+        permissions = "u=rwx,g=,o=";
+        capabilities = "cap_dac_read_search=+ep";
+      };
 
-    services.restic.backups = {
-      remotebackup = {
-        initialize = true;
-        paths = cfg.stateDirs;
-        repository = "sftp::/home/backup";
-        extraOptions = [
-          "sftp.command='ssh u463932@u463932.your-storagebox.de -p 23 -i ${
-            config.sops.secrets."n100/restic/id_hetzner".path
-          } -s sftp'"
-        ];
-        passwordFile = config.sops.secrets."n100/restic/password".path;
-        pruneOpts = [ "--keep-daily 7" ];
-        timerConfig = { OnCalendar = "00:01"; };
-        backupPrepareCommand =
-          builtins.concatStringsSep "\n" cfg.preBackupScripts;
-        backupCleanupCommand =
-          builtins.concatStringsSep "\n" cfg.postBackupScripts;
+      services.restic.backups = {
+        remotebackup = {
+          initialize = true;
+          paths = cfg.stateDirs;
+          repository = "sftp::/home/backup";
+          extraOptions = [
+            "sftp.command='ssh u463932@u463932.your-storagebox.de -p 23 -i ${
+              config.sops.secrets."n100/restic/id_hetzner".path
+            } -s sftp'"
+          ];
+          passwordFile = config.sops.secrets."n100/restic/password".path;
+          pruneOpts = [ "--keep-daily 7" ];
+          timerConfig = { OnCalendar = "00:01"; };
+          backupPrepareCommand =
+            builtins.concatStringsSep "\n" cfg.preBackupScripts;
+          backupCleanupCommand =
+            builtins.concatStringsSep "\n" cfg.postBackupScripts;
+        };
       };
-    };
-  } // (lib.mkIf hcfg.email.enable {
-    systemd.services.restic-remotebackup-email = {
-      description = "Restic Check and Email Results";
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${emailScript}";
+    })
+    (lib.mkIf hcfg.email.enable {
+      systemd.services.restic-remotebackup-email = {
+        description = "Restic Check and Email Results";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${emailScript}";
+        };
       };
-    };
 
-    systemd.timers.restic-remotebackup-email = {
-      description = "Weekly Restic Check Email Timer";
-      wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnCalendar = "Sun 10:00";
-        Persistent = true;
+      systemd.timers.restic-remotebackup-email = {
+        description = "Weekly Restic Check Email Timer";
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnCalendar = "Sun 10:00";
+          Persistent = true;
+        };
       };
-    };
-  });
+    })
+  ];
 }
