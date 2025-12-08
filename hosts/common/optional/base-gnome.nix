941bf679f165a24695f3521a069ed11a9552ff9f
Levente Berky <levente.berky@gmail.com>
2025-12-08 19:03:36 +0100

    update gnome config to 25.11

--- hosts/common/optional/base-gnome.nix
+++ hosts/common/optional/base-gnome.nix
@@ -1,52 +1,13 @@
 { pkgs, ... }:
 {
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages = [
-    pkgs.baobab      # disk usage analyzer
-    pkgs.cheese      # photo booth
-    pkgs.eog         # image viewer
-    pkgs.epiphany    # web browser
-    # pkgs.simple-scan # document scanner
-    pkgs.totem       # video player
-    pkgs.yelp        # help viewer
-    pkgs.evince      # document viewer
-    pkgs.geary       # email client
-    pkgs.seahorse    # password manager
 
-    # these should be self explanatory
-    # pkgs.gnome-calculator
-    pkgs.gnome-calendar
-    pkgs.gnome-characters
-    pkgs.gnome-clocks
-    pkgs.gnome-contacts
-    pkgs.gnome-font-viewer
-    pkgs.gnome-logs
-    pkgs.gnome-maps
-    pkgs.gnome-music 
-    pkgs.gnome-system-monitor
-    pkgs.gnome-connections
-  ];
-  programs.dconf.enable = true;
-
-
-  environment.systemPackages = with pkgs; [
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly
-    gst_all_1.gst-libav
-    gst_all_1.gst-vaapi
-  ];
-
-  environment.variables = {
-    GST_PLUGIN_PATH = "/run/current-system/sw/lib/gstreamer-1.0/";
-  };
+  # Disable default apps
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -54,6 +15,6 @@
     variant = "";
   };
 
-  # Hint electron apps to use wayland:
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  # Hint electron apps to use wayland: (should not be needed?)
+  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
